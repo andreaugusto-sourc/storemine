2d0756c1f43cac3bc5977df95dc41714b409c540
andreaugusto-sourc <andreaugusto1033@gmail.com>
2022-04-09 10:32:19 -0300

    pequenas correções

--- footer.php
+++ footer.php
@@ -21,4 +21,53 @@
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
 <script src="js/menu.js"></script>
+<style>
+/* FOOTER */
+footer {
+    padding: 0.5rem 0rem;
+    font-size: 1.8rem;
+    margin-top: 2%;
+    display: flex;
+    border-top: var(--cor3) 0.2rem solid;
+    width: 100%;
+    align-items: center;
+    justify-content: space-evenly;
 
+}
+
+#redes-sociais {
+    justify-content: space-around;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+
+}
+
+#redes-sociais a {
+    color: black;
+    margin-top: 1rem;
+    text-align: center;
+}
+
+.redes-icons {
+    padding: 1rem;
+    color: black;
+    font-size: 5rem;
+}
+
+#caminhos-footer {
+    flex-direction: column;
+    justify-content: flex-end;
+    display: flex;
+    align-items: center;
+
+}
+
+.item-rodape {
+    color: var(--cor4);
+    text-decoration: none;
+    padding: 2rem 4rem 2rem 4rem;
+
+}
+</style>
